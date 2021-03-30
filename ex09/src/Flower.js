843ff03b606bad2b0eecf19dacbd2f366db91a6f
unknown <hamza.okanovic@hotmail.com>
2021-03-30 03:12:19 +0200

    Added new files

--- ex09/src/Flower.js
+++ ex09/src/Flower.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Flower extends Component {
     render() {
         return (
-            <h2>This is a {this.props.color} Flower</h2>
+            <h2>This is a {this.props.color} Flower!</h2>
         )
     }
 }
